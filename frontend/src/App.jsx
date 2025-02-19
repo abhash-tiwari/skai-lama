0c37f2f84d5687b67941bc753eb3ffb862e3a0b1
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-19 18:46:58 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     I have been creating the backend functionality from last night but forgot to push codes so after completing the whole backend code I push the backend with managing a good commit history
+    </>
+  )
+}
+
+export default App
